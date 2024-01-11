446aabac64d9d27238d5cc1059d25c64db367cc9
eden-hason <eden@hason.me>
2024-01-11 10:46:44 +0200

    feat: add link to the new dictation page

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@
 
 import { useContext } from 'react';
 import { AuthContext } from './providers/AuthProvider';
+import { Button } from '@nextui-org/button';
+import { FaPlus } from 'react-icons/fa';
+import { Link } from '@nextui-org/link';
 
 export default function Home() {
   const { user } = useContext(AuthContext);
@@ -9,8 +12,18 @@ export default function Home() {
   if (!user) return null;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-4">
       <h1>Hello world!</h1>
+      <div className="w-full">
+        <Button
+          as={Link}
+          href="pages/dictation/new"
+          color="primary"
+          variant="light"
+          startContent={<FaPlus />}>
+          הכתבה חדשה
+        </Button>
+      </div>
     </main>
   );
 }
