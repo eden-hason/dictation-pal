23356ecc10dad63a05bfce987461c84a05d8be78
eden-hason <eden@hason.me>
2024-01-21 18:46:34 +0200

    feat: add beta chip

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,28 +1,30 @@
-import {
-  Navbar as _Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Link,
-  Button,
-} from '@nextui-org/react';
+import { Navbar as _Navbar, NavbarBrand, Link, Chip } from '@nextui-org/react';
 
 const Navbar = ({}) => {
   return (
     <_Navbar className="bg-slate-50">
       <NavbarBrand as={Link} href="/">
-        <p className="font-bold text-black">
-          Dictation <span className="text-purple-400">Pal</span>
-        </p>
+        <div class="flex items-center">
+          <Chip
+            className="ml-1 text-xs h-4"
+            color="primary"
+            size="sm"
+            variant="flat">
+            Beta
+          </Chip>
+          <p className="font-bold text-black">
+            Dictation <span className="text-purple-400">Pal</span>
+          </p>
+        </div>
       </NavbarBrand>
 
-      <NavbarContent justify="end">
+      {/* <NavbarContent justify="end">
         <NavbarItem>
           <Button as={Link} size="sm" color="primary" href="#" variant="flat">
             הרשמה
           </Button>
         </NavbarItem>
-      </NavbarContent>
+      </NavbarContent> */}
     </_Navbar>
   );
 };
